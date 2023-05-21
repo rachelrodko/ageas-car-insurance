1ce017bff9ec5a898828844f6e3f92a62820c76e
rachelrodko <107275627+rachelrodko@users.noreply.github.com>
2023-05-21 18:02:40 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const sectionHeroEl = document.querySelector(".header");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
